287f56590d8529928b06d332e146e597fc9a1fd7
Isaac Sunoo <isaacsunoo@gmail.com>
2019-04-14 23:18:29 -0600

    added testing for thunks

---
+++ src/thunks/__tests__/index.js
@@ -0,0 +1,209 @@
+import { getDailyNews, getApollo20, getOrion, getMilkyway, getNebula, getGalaxy } from '../index';
+import * as actions from '../../actions';
+
+describe('Thunks', () => {
+    let mockDispatch;
+    let mockData;
+    beforeEach(() => {
+        mockDispatch = jest.fn();
+        mockData = ['NASA'];
+        window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+            ok: true,
+            json: () => Promise.resolve({
+                mockData,
+            })
+        }));
+    });
+
+    describe('getDaily', () => {
+        let thunk;
+        beforeEach(() => {
+            thunk = getDailyNews();
+        });
+
+        it('calls fetch', () => {
+            thunk(mockDispatch);
+            expect(window.fetch).toHaveBeenCalled();
+        });
+
+        it('calls dispatch with setLoading', () => {
+            thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(true));
+        });
+
+        it('should dispatch setLoading to false', async () => {
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(false));
+        });
+
+        it.skip('should dispatch setError with error message if the response is not ok', async () => {
+            window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+                ok: false,
+                statusText: 'Error Retrieving'
+            }));
+            const thunk = getDailyNews;
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setError('Error Retrieving.'))
+        });
+    });
+
+    describe('getApollo', () => {
+        let thunk;
+        beforeEach(() => {
+            thunk = getApollo20();
+        });
+
+        it('calls fetch', () => {
+            thunk(mockDispatch);
+            expect(window.fetch).toHaveBeenCalled();
+        });
+
+        it('calls dispatch with setLoading', () => {
+            thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(true));
+        });
+
+        it('should dispatch setLoading to false', async () => {
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(false));
+        });
+
+        it.skip('should dispatch setError with error message if the response is not ok', async () => {
+            window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+                ok: false,
+                statusText: 'Error Retrieving'
+            }));
+            const thunk = getApollo20;
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setError('Error Retrieving.'))
+        });
+    });
+
+    describe('getOrion', () => {
+        let thunk;
+        beforeEach(() => {
+            thunk = getOrion();
+        });
+
+        it('calls fetch', () => {
+            thunk(mockDispatch);
+            expect(window.fetch).toHaveBeenCalled();
+        });
+
+        it('calls dispatch with setLoading', () => {
+            thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(true));
+        });
+
+        it('should dispatch setLoading to false', async () => {
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(false));
+        });
+
+        it.skip('should dispatch setError with error message if the response is not ok', async () => {
+            window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+                ok: false,
+                statusText: 'Error Retrieving'
+            }));
+            const thunk = getOrion;
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setError('Error Retrieving.'))
+        });
+    });
+
+    describe('getMilkyway', () => {
+        let thunk;
+        beforeEach(() => {
+            thunk = getMilkyway();
+        });
+
+        it('calls fetch', () => {
+            thunk(mockDispatch);
+            expect(window.fetch).toHaveBeenCalled();
+        });
+
+        it('calls dispatch with setLoading', () => {
+            thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(true));
+        });
+
+        it('should dispatch setLoading to false', async () => {
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(false));
+        });
+
+        it.skip('should dispatch setError with error message if the response is not ok', async () => {
+            window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+                ok: false,
+                statusText: 'Error Retrieving'
+            }));
+            const thunk = getDailyNews;
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setError('Error Retrieving.'))
+        });
+    });
+
+    describe('getNebula', () => {
+        let thunk;
+        beforeEach(() => {
+            thunk = getNebula();
+        });
+
+        it('calls fetch', () => {
+            thunk(mockDispatch);
+            expect(window.fetch).toHaveBeenCalled();
+        });
+
+        it('calls dispatch with setLoading', () => {
+            thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(true));
+        });
+
+        it('should dispatch setLoading to false', async () => {
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(false));
+        });
+
+        it.skip('should dispatch setError with error message if the response is not ok', async () => {
+            window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+                ok: false,
+                statusText: 'Error Retrieving'
+            }));
+            const thunk = getNebula;
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setError('Error Retrieving.'))
+        });
+    });
+
+    describe('getGalaxy', () => {
+        let thunk;
+        beforeEach(() => {
+            thunk = getGalaxy();
+        });
+
+        it('calls fetch', () => {
+            thunk(mockDispatch);
+            expect(window.fetch).toHaveBeenCalled();
+        });
+
+        it('calls dispatch with setLoading', () => {
+            thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(true));
+        });
+
+        it('should dispatch setLoading to false', async () => {
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setLoading(false));
+        });
+
+        it.skip('should dispatch setError with error message if the response is not ok', async () => {
+            window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+                ok: false,
+                statusText: 'Error Retrieving'
+            }));
+            const thunk = getGalaxy;
+            await thunk(mockDispatch);
+            expect(mockDispatch).toHaveBeenCalledWith(actions.setError('Error Retrieving.'))
+        });
+    });
+})
