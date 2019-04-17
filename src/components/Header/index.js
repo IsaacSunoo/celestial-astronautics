2ff92f11473ba44f038665e6e1004b093e949650
Isaac Sunoo <isaacsunoo@gmail.com>
2019-04-16 23:07:08 -0600

    added a sticky header to application

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import DropdownMenu from '../DropdownMenu';
+import 'react-sticky-header/styles.css';
+import StickyHeader from 'react-sticky-header';
 
 const Header = () => {
+
   return (
-    <header className='header-container'>
-      <h1>Celestial Astronautical</h1>
-      <section className='route-container'>
-        <DropdownMenu />
-      </section>
-    </header>
-  )
+    <StickyHeader
+      header={
+        <header className="header-container">
+          <h1>Celestial Astronautical</h1>
+          <section className="route-container">
+            <DropdownMenu />
+          </section>
+        </header>
+      }
+    />
+  );
 }
 
 export default Header;
